5a52ae96bf0a9aa38527cb817491d0206033ad6b
Christer van der Meeren <cmeeren@protonmail.com>
2017-11-02 08:33:16 +0100

    Add tests

--- tests/Hedgehog.Experimental.Tests/GenTests.fs
+++ tests/Hedgehog.Experimental.Tests/GenTests.fs
@@ -2,7 +2,121 @@
 
 open Xunit
 open Swensen.Unquote
+open Hedgehog
 
 [<Fact>]
-let ``TODO`` () =
-    test <@ true @>
+let ``notIn generates element that is not in list`` () =
+    Property.check <| property {
+        let! xs =
+            Gen.int (Range.linearFrom 0 -100 100)
+            |> Gen.list (Range.linear 1 10)
+        let! x = Gen.int (Range.linearFrom 0 -100 100) |> Gen.notIn xs
+        return not <| List.contains x xs
+    }
+
+[<Fact>]
+let ``notContains generates list that does not contain element`` () =
+    Property.check <| property {
+        let! x = Gen.int (Range.linearFrom 0 -100 100)
+        let! xs =
+            Gen.int (Range.linearFrom 0 -100 100)
+            |> Gen.list (Range.linear 1 10)
+            |> Gen.notContains x
+        return not <| List.contains x xs
+    }
+
+[<Fact>]
+let ``sorted2 generates a sorted 2-tuple`` () =
+    Property.check <| property {
+        let! x1, x2 =
+            Gen.int (Range.exponentialBounded())
+            |> Gen.tuple
+            |> Gen.sorted2
+        x1 <=! x2
+    }
+
+[<Fact>]
+let ``sorted3 generates a sorted 3-tuple`` () =
+    Property.check <| property {
+        let! x1, x2, x3 =
+            Gen.int (Range.exponentialBounded())
+            |> Gen.tuple3
+            |> Gen.sorted3
+        x1 <=! x2
+        x2 <=! x3
+    }
+
+[<Fact>]
+let ``sorted4 generates a sorted 4-tuple`` () =
+    Property.check <| property {
+        let! x1, x2, x3, x4 =
+            Gen.int (Range.exponentialBounded())
+            |> Gen.tuple4
+            |> Gen.sorted4
+        x1 <=! x2
+        x2 <=! x3
+        x3 <=! x4
+    }
+
+[<Fact>]
+let ``distinct2 generates 2 non-equal elements`` () =
+    Property.check <| property {
+        let! x1, x2 =
+            Gen.int (Range.exponentialBounded())
+            |> Gen.tuple
+            |> Gen.distinct2
+        [x1; x2] |> List.distinct =! [x1; x2]
+    }
+
+[<Fact>]
+let ``distinct3 generates 3 non-equal elements`` () =
+    Property.check <| property {
+        let! x1, x2, x3 =
+            Gen.int (Range.exponentialBounded())
+            |> Gen.tuple3
+            |> Gen.distinct3
+        [x1; x2; x3] |> List.distinct =! [x1; x2; x3]
+    }
+
+[<Fact>]
+let ``distinct4 generates 4 non-equal elements`` () =
+    Property.check <| property {
+        let! x1, x2, x3, x4 =
+            Gen.int (Range.exponentialBounded())
+            |> Gen.tuple4
+            |> Gen.distinct4
+        [x1; x2; x3; x4] |> List.distinct =! [x1; x2; x3; x4]
+    }
+
+[<Fact>]
+let ``increasing2 generates a 2-tuple with strictly increasing elements`` () =
+    Property.check <| property {
+        let! x1, x2 =
+            Gen.int (Range.exponentialBounded())
+            |> Gen.tuple
+            |> Gen.increasing2
+        x1 <! x2
+    }
+
+[<Fact>]
+let ``increasing3 generates a 3-tuple with strictly increasing elements`` () =
+    Property.check <| property {
+        let! x1, x2, x3 =
+            Gen.int (Range.exponentialBounded())
+            |> Gen.tuple3
+            |> Gen.increasing3
+        x1 <! x2
+        x2 <! x3
+    }
+
+[<Fact>]
+let ``increasing4 generates a 4-tuple with strictly increasing elements`` () =
+    Property.check <| property {
+        let! x1, x2, x3, x4 =
+            Gen.int (Range.exponentialBounded())
+            |> Gen.tuple4
+            |> Gen.increasing4
+        x1 <! x2
+        x2 <! x3
+        x3 <! x4
+    }
